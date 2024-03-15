d0b1bff4f58b70ad3f57c0611565df7aa2c88c3e
GreeMII <gleb_cry@mail.ru>
2024-03-15 17:58:30 +0300

    WIP

--- crates/cicero-app/src/pages/template_choice.rs
+++ crates/cicero-app/src/pages/template_choice.rs
@@ -23,14 +23,14 @@ pub fn TemplateChoice() -> impl IntoView {
             <div id="main_body">
                 <div id="left_side" class="md:flex md:items-center w-1/2">
                     <section id="search" class="w-full h-[73px] bg-[#EEEEEE]">
-                        <div class="justify-between h-[37px] mt-[18px] mb-[18px] ml-[25px] mr-[25px] relative">
+                        <div class="justify-between items-center h-[37px] mt-[18px] mb-[18px] ml-[25px] mr-[25px] relative">
                             <input
                                 type="search"
-                                class="w-full h-full outline-none bg-[#261201] bg-opacity-[81%] border-solid border-[3px] rounded-[10px] border-[#8C7456] placeholder-[#A1A1A1] text-[16px] font-light pl"
+                                class="w-full pl-[30px] h-full absolute outline-none bg-[#261201] bg-opacity-[81%] border-solid border-[3px] rounded-[10px] border-[#8C7456] placeholder-[#A1A1A1] text-[#A1A1A1] text-[16px] font-light pl"
                                 name="search-text"
                                 placeholder="Поиск документов"
                             />
-                            <i class="bx bx-search text-[#8C7456] text-[25px] absolute"></i>
+                            <i class="bx bx-search items-center pt-[8px] pl-[7px] text-[#8C7456] text-[25px] absolute"></i>
                         </div>
                     </section>
                     <section id="find_doc"></section>
