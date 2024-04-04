ad21f37ad4a28fb4dd6d63e96796033cb4e44bf9
GreeMII <gleb_cry@mail.ru>
2024-04-04 19:38:19 +0300

    fix(cicero-app): WIP
    Delete a main body scrollbar, because on this moment input_data scrollbar not work

--- crates/cicero-app/src/pages/scenario.rs
+++ crates/cicero-app/src/pages/scenario.rs
@@ -31,7 +31,7 @@ pub fn Scenario() -> impl IntoView {
                 </section>
                 <section
                     id="input_data"
-                    class="flex flex-col flex-1 h-full bg-[#EEEEEE] border-r-[7px] border-[#8c7456]"
+                    class="flex flex-col flex-1 h-[100%] bg-[#EEEEEE] border-r-[7px] border-[#8c7456]"
                 >
                     <section
                         id="warning"
@@ -44,8 +44,104 @@ pub fn Scenario() -> impl IntoView {
                             Cмотрите ст.13 Конституции РФ
                         </div>
                     </section>
-                    <section class="flex flex-col w-full scrollbar-thumb-[#8C7456] scrollbar-thumb-h-[40px] scrollbar-track-[#eeeeee] hover:scrollbar-thumb-[#9c805d] active:scrollbar-thumb-[#9c805d]/50 scrollbar h-[90%] scrollbar-w-[40px] overflow-y-scroll">
-                        <section class="flex flex-col text-[#8c7456] w-full h-full px-[15px] py-[30px]">
+                    <section class="flex flex-col w-full h-full scrollbar-thumb-[#8C7456] scrollbar-thumb-h-[40px] scrollbar-track-[#eeeeee] hover:scrollbar-thumb-[#9c805d] active:scrollbar-thumb-[#9c805d]/50 scrollbar-h-[90%] scrollbar-w-[40px] overflow-y-scroll">
+                        <section class="flex flex-col text-[#8c7456] w-full h-full px-[15px] pb-[15px]">
+                            <div class="flex flex-col gap-[10px] mb-[20px]">
+                                <div class="font-bold">
+                                    Введите данные покупателя:
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Имя:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Имя"
+                                        required
+                                    />
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Возраст:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Возраст"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-[10px] mb-[20px]">
+                                <div class="font-bold">
+                                    Введите данные покупателя:
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Имя:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Имя"
+                                        required
+                                    />
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Возраст:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Возраст"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </section>
+                        <section class="flex flex-col text-[#8c7456] w-full h-full px-[15px] pb-[15px]">
+                            <div class="flex flex-col gap-[10px] mb-[20px]">
+                                <div class="font-bold">
+                                    Введите данные покупателя:
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Имя:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Имя"
+                                        required
+                                    />
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Возраст:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Возраст"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-[10px] mb-[20px]">
+                                <div class="font-bold">
+                                    Введите данные покупателя:
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Имя:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Имя"
+                                        required
+                                    />
+                                </div>
+                                <div class="pl-[25px] flex flex-row gap-x-[5px] items-center">
+                                    Возраст:
+                                    <input
+                                        class="bg-[#eeeeee] appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#eeeeee] focus:border-[#8c7456]"
+                                        type="text"
+                                        placeholder="Возраст"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </section>
+                        <section class="flex flex-col text-[#8c7456] w-full h-full px-[15px] pb-[15px]">
                             <div class="flex flex-col gap-[10px] mb-[20px]">
                                 <div class="font-bold">
                                     Введите данные покупателя:
